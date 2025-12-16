841b89a7db4f924fa4277fa6b3494d053cc674c1
sytseng <shihyitseng41@gmail.com>
2025-12-15 23:08:27 -0800

    comment out optogenetic module imports

--- src/spyglass/common/__init__.py
+++ src/spyglass/common/__init__.py
@@ -42,13 +42,13 @@ from spyglass.common.common_interval import (
 )
 from spyglass.common.common_lab import Institution, Lab, LabMember, LabTeam
 from spyglass.common.common_nwbfile import AnalysisNwbfile, Nwbfile
-from spyglass.common.common_optogenetics import (
-    OpticalFiberDevice,
-    OpticalFiberImplant,
-    OptogeneticProtocol,
-    Virus,
-    VirusInjection,
-)
+# from spyglass.common.common_optogenetics import (
+#     OpticalFiberDevice,
+#     OpticalFiberImplant,
+#     OptogeneticProtocol,
+#     Virus,
+#     VirusInjection,
+# )
 from spyglass.common.common_position import (
     IntervalLinearizationSelection,
     IntervalLinearizedPosition,
@@ -65,7 +65,7 @@ from spyglass.common.common_session import Session
 from spyglass.common.common_subject import Subject
 from spyglass.common.common_task import Task, TaskEpoch
 from spyglass.common.common_user import UserEnvironment
-from spyglass.common.populate_all_common import populate_all_common
+# from spyglass.common.populate_all_common import populate_all_common
 from spyglass.common.prepopulate import populate_from_yaml, prepopulate_default
 from spyglass.settings import prepopulate
 from spyglass.utils.nwb_helper_fn import (
